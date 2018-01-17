10aa64fb8b87d3e30d6183736a3b2a1793855434
rubiktwjack <rubiktwjack@gmail.com>
2018-01-17 16:52:09 +0800

    snow

---
+++ content.js
@@ -0,0 +1,8 @@
+var canvas = document.createElement("canvas");  
+var cover = document.getElementsByClassName("coverBorder");  
+canvas.id = "canvasLaLa2016";  
+cover[0].appendChild(canvas);  
+
+var button = document.createElement("div");  
+button.id = "snow-button";  
+cover[0].appendChild(button);
