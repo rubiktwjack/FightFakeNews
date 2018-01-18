d1c710fe4e762d497d866ad788194e04c1c82b02
rubiktwjack <rubiktwjack@gmail.com>
2018-01-18 19:58:45 +0800

    tst

--- event.js
+++ event.js
@@ -1,5 +1,33 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    console.log(message);
-    console.log(sender);
-    sendResponse({content: "來自事件腳本的回覆"});
+var toggle = false;  
+//可以列出禁止防問的黑名單網址  
+var blockList = [];  
+
+chrome.runtime.onMessageExternal.addListener(function(message, sender, sendResponse) {  
+    console.log(sender);  
+
+    //如果訪問在黑名單，就不作任何動作  
+    if (blockList.indexOf(sender.url) != -1) {  
+        return;  
+    }  
+
+    if (message.name != "切換頁面按鈕") {  
+        return;  
+    }  
+
+    //如果按鈕是啟用的狀態則開，否則關  
+    if (!toggle) {  
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {  
+            chrome.pageAction.show(tabs[0].id);  
+            toggle = !toggle;  
+        });  
+    } else {  
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {  
+            chrome.pageAction.hide(tabs[0].id);  
+            toggle = !toggle;  
+
+        });  
+    }  
+
+    sendResponse("來自擴充功能的訊息：操作完成");  
+
 });
